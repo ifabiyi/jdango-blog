c6a3766a38f1ac452772cb030bdfca2bebe2e773
Ifabiyi <ifabiyimo@gmail.com>
2020-06-01 16:42:12 +0100

    created and updated user profile

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': ' Mayowa',
-        'title': 'First blog post',
-        'content': 'Awww and  here comes the first',
-        'date': 'June 23 2020' 
-    },
-
-    {
-        'author': ' Ifabiyi',
-        'title': 'Second blog post',
-        'content': 'Awww and  here comes the second',
-        'date': 'June 24 2020' 
-    }
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
