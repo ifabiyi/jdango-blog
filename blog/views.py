a1be2bbfcd4ebc986efc0f0c0b4dd77cb9dccba0
Ifabiyi <ifabiyimo@gmail.com>
2020-06-03 00:27:20 +0100

    crud part

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+#for class based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -9,6 +12,38 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+#class based view without sticking to the convention
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'   #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    #change the order of the post tothe latest
+    ordering = ['-date_posted']
+
+#blog detailed view  based on convention
+class PostDetailView(DetailView):
+    model = Post
+    
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
